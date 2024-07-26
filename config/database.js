9b5ed895b78541462cba5dc49908714db6781776
G M Ganesh <gmganesh173@gmail.com>
2024-07-26 11:48:11 +0530

    DB Changes

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ConnectedDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_DB_URL);
     console.log("Database Connected");
   } catch (error) {
     console.log(error);
